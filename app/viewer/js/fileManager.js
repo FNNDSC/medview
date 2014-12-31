8ad9e6f8ef892fd19ac2d35e88dce8005e2f2f6f
Jorge <jbernal0019@yahoo.es>
2014-12-31 11:51:17 -0500

    added requestFileSystem and fsErrorHandler

--- app/viewer/js/fileManager.js
+++ app/viewer/js/fileManager.js
@@ -20,19 +20,65 @@ var fileManager = fileManager || {};
   fileManager.FileManager = function() {
 
     this.version = 0.0;
-    this.id = '';
+    // local filesystem object
+    this.fs = null;
 
   }
 
 
-  fileManager.FileManager.prototype.isInCloud = function(file){}
+  fileManager.FileManager.prototype.requestFileSystem = function(file){
+    var self = this;
 
-  fileManager.FileManager.prototype.cloudUpload = function(fileArray){}
+    // The file system has been prefixed as of Google Chrome 12:
+    window.requestFileSystem = window.requestFileSystem || window.webkitRequestFileSystem;
+    // Request 5GB
+    /*window.webkitStorageInfo.requestQuota( PERSISTENT, 5*1024*1024*1024, function(grantedBytes) {
+      window.requestFileSystem(PERSISTENT, grantedBytes, function(fs){self.fs = fs;}, self.fsErrorHandler);
+    }, function(e) {
+      console.log('Error', e);} ); */
+    if (window.requestFileSystem) {
+      window.requestFileSystem(window.TEMPORARY, 5*1024*1024*1024, function(fs){
+        self.fs = fs;
+      }, self.fsErrorHandler);
+    }
+  }
 
-  fileManager.FileManager.prototype.cloudDownload = function(fileArray){}
 
-  fileManager.FileManager.prototype.isLocalFile = function(file){}
+  fileManager.FileManager.prototype.fsErrorHandler = function(fe) {
+    var msg = '';
 
-  fileManager.FileManager.prototype.readLocalFile = function(fileArray){}
+    switch (fe.code) {
+      case FileError.QUOTA_EXCEEDED_ERR:
+        msg = 'QUOTA_EXCEEDED_ERR';
+        break;
+      case FileError.NOT_FOUND_ERR:
+        msg = 'NOT_FOUND_ERR';
+        break;
+      case FileError.SECURITY_ERR:
+        msg = 'SECURITY_ERR';
+        break;
+      case FileError.INVALID_MODIFICATION_ERR:
+        msg = 'INVALID_MODIFICATION_ERR';
+        break;
+      case FileError.INVALID_STATE_ERR:
+        msg = 'INVALID_STATE_ERR';
+        break;
+      default:
+        msg = 'Unknown Error';
+        break;
+    };
+    console.log('Error: ' + msg);
+  }
 
-  fileManager.FileManager.prototype.writeLocalFile = function(fileArray){}
+
+  fileManager.FileManager.prototype.isLocalFile = function(file) {}
+
+  fileManager.FileManager.prototype.readLocalFile = function(fileArray) {}
+
+  fileManager.FileManager.prototype.writeLocalFile = function(fileArray) {}
+
+  fileManager.FileManager.prototype.isInCloud = function(file) {}
+
+  fileManager.FileManager.prototype.cloudUpload = function(fileArray) {}
+
+  fileManager.FileManager.prototype.cloudDownload = function(fileArray) {}
