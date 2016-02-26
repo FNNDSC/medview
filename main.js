353e6d151c5d8a6a041e65c8883082d178de39f5
Jorge <jbernal0019@yahoo.es>
2016-02-26 15:32:37 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,1 @@
+require.config({baseUrl:".",paths:{jquery:["https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min","libs/jquery/dist/jquery.min"],jquery_ui:["https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min","libs/jquery-ui/jquery-ui.min"],medview:"js/medview.min"}}),require(["medview"],function(a){var b=new a.App;b.init()});
